a4cd83f1303ff07f03c8691cb72e0ba73ad83ec7
David Valles <davidtjvalles@gmail.com>
2021-02-07 15:11:36 -0800

    Use lint-staged config file

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // type-check typescript files
+  '**/*.ts?(x)': () => 'tsc --pretty --noEmit',
+
+  // lint all code
+  '**/*.(ts|js)?(x)': (filenames) => `eslint ${filenames.join(' ')}`,
+
+  // format all files
+  '**/*': (filenames) =>
+    filenames.map((filename) => `prettier --write '${filename}'`),
+}
