6d6a0aae0c01f5d67d48527ce1c42044c1778ce4
David Valles <davidtjvalles@gmail.com>
2021-02-07 16:10:29 -0800

    Reorder pre-commit tasks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  // type-check typescript files
-  '**/*.ts?(x)': () => 'tsc --pretty --noEmit',
-
-  // lint all code
-  '**/*.(ts|js)?(x)': (filenames) => `eslint ${filenames.join(' ')}`,
-
   // format all files
   '**/*': (filenames) =>
     filenames.map(
       (filename) =>
         `prettier --config ./.prettierrc.js --write '${filename}' --ignore-unknown`
     ),
+
+  // lint all code
+  '**/*.(ts|js)?(x)': (filenames) => `eslint ${filenames.join(' ')}`,
+
+  // type-check typescript files
+  '**/*.ts?(x)': () => 'tsc --pretty --noEmit',
 }
