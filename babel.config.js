b2ea693fdd36ef1b355b54351e85399c714cbacd
David Valles <davidtjvalles@gmail.com>
2021-03-11 15:18:31 -0800

    Update jest config. Add babel config comment

--- babel.config.js
+++ babel.config.js
@@ -1,2 +1,3 @@
+// next/babel preset comes with next.js
 const presets = ['next/babel']
 module.exports = { presets }
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+
+  // i set `baseUrl` in `tsconfig.js` (https://stackoverflow.com/a/51174924).
+  moduleDirectories: ['node_modules', 'src'],
 }
