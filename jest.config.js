10eba659de58dd8d170c1518fe0a73079d845419
David Valles <davidtjvalles@gmail.com>
2021-03-11 20:17:18 -0800

    Fix module resolution issue

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-
-  // i set `baseUrl` in `tsconfig.js` (https://stackoverflow.com/a/51174924).
-  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@/hooks/(.*)': '<rootDir>/hooks/$1',
+    '@/layouts/(.*)': '<rootDir>/layouts/$1',
+    '@/components/(.*)': '<rootDir>/components/$1',
+  },
 }
